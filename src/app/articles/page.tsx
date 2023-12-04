080ca5682a3c436cb4749fecba419125c9fd888e
宮内 優輔 <yusuke.miyauchi.vt@9105-109985.local>
2023-12-04 18:15:58 +0900

    update: ダミーコンポーネント表示箇所とarticleコンポーネントを更新

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Link from 'next/link';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 export default function Page() {
   return (
-    <>
-      <div>articles</div>
-    </>
+    <Box>
+      <Card>
+        <CardHeader title='記事タイトル(dummy)' subheader='2023/12/1' />
+        <CardContent>
+          <Link href='/hoge'>
+            <CardMedia
+              component='img'
+              width='100%'
+              height='100%'
+              image='https://source.unsplash.com/collection/1346951/1000x500?sig=2'
+              alt='Paella dish'
+            />
+          </Link>
+          <Typography variant='body1' color='text.secondary' sx={{ paddingTop: '1em' }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quod maiores totam aperiam necessitatibus
+            est, exercitationem architecto quibusdam accusamus maxime asperiores eaque adipisci itaque nemo, minus odio,
+            alias dolor possimus!
+          </Typography>
+        </CardContent>
+      </Card>
+    </Box>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import Box from '@mui/material/Box';
+import Article from '../app/articles/page';
+import DummyArticle from '../app/components/DummyArticle';
+
 export default function Home() {
-  return <div>記事投稿</div>;
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'space-between', margin: '2em' }}>
+      <Box sx={{ width: '70%' }}>
+        <Article />
+      </Box>
+      <Box>
+        <DummyArticle />
+      </Box>
+    </Box>
+  );
 }
