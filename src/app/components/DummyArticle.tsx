8081ee48bc868d3ff27572a6731168a66fef37c8
宮内 優輔 <yusuke.miyauchi.vt@9105-109985.local>
2023-12-05 14:15:03 +0900

    update: testidを追加

--- src/app/components/DummyArticle.tsx
+++ src/app/components/DummyArticle.tsx
@@ -1,12 +1,12 @@
 import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
 export default function DummyArticle() {
   return (
-    <Card>
+    <Card data-testid='dummy-article'>
       <CardHeader title='記事タイトル(dummy)' subheader='2023/12/1' />
       <CardContent>
         <CardMedia component='img' height='194' image='/assets/dummy-blog-image.jpg' alt='Paella dish' />
@@ -15,7 +15,7 @@ export default function DummyArticle() {
           <br />
           右上の「記事を書く」ボタンから新規記事を執筆してください。
         </Typography>
-        <Typography variant='body1' color='text.secondary' sx={{ paddingTop: '1em' }}>
+        <Typography variant='body1' color='text.secondary' sx={{ paddingTop: '1em' }} data-testid='dummy-article--text'>
           ※ &thinsp; 記事投稿後ダミー記事は非表示となります。
         </Typography>
       </CardContent>
