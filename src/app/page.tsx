7153828d31b5acb6f1c7a5d675218d5be8078ca9
宮内 優輔 <yusuke.miyauchi.vt@9105-109985.local>
2023-12-05 14:15:49 +0900

    update: ページコンポーネントの構成を変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import Box from '@mui/material/Box';
 import Article from '../app/articles/page';
-import DummyArticle from '../app/components/DummyArticle';
 
 export default function Home() {
-  return (
-    <Box sx={{ display: 'flex', justifyContent: 'space-between', margin: '2em' }}>
-      <Box sx={{ width: '70%' }}>
-        <Article />
-      </Box>
-      <Box>
-        <DummyArticle />
-      </Box>
-    </Box>
-  );
+  return <Article />;
 }
